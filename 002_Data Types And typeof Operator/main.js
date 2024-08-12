ac916dbaa0af92966d21999f51866932559dccfa
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-08-12 17:56:08 +0300

    Data Types And typeof Operator

---
+++ 002_Data Types And typeof Operator/main.js
@@ -0,0 +1,22 @@
+/*
+    Data Types 
+    - Number => 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    - String => "Hello World"
+    - Boolean => true, false
+    - Array => Object => [1, 2, 3, 4, 5] / ["John", "Mohamed"]
+    - Object => {name: "John Doe", age: 25}
+    - Null => null
+    - Undefined => undefined
+*/
+
+console.log(typeof("Mohamed Badwy"));
+console.log(typeof "Mohamed Badwy");
+console.log(typeof 100);
+console.log(typeof 100.99);
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof [1, 2, 3]);
+console.log(typeof ["Ahmed", "Mohamed"]);
+console.log(typeof {name: "Mohamed", age: 24});
+console.log(typeof undefined);
+console.log(typeof null);
