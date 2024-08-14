34c93fe8dea2cc1946e7d3820ff8c6c17a3b77c4
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-08-14 14:49:43 +0300

    Concatenation

--- 006_Concatenation/script.js
+++ 006_Concatenation/script.js
@@ -1,5 +1,11 @@
 /*
     * Concatenation *
+    -------------------
+    - Concatenation is the process of combining two strings.
+    - The + operator is used to concatenate two strings.
+    - The concat() method is also used to concatenate two strings.
+    - The concat() method can be used with multiple arguments.
+    - The concat() method can be used with variables.
 */
 
 let a = "We Love";
