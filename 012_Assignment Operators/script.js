b12af8c1e35726fc832f9905a6226063a601bb1c
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-08-14 20:06:16 +0300

    Assignment Operators

---
+++ 012_Assignment Operators/script.js
@@ -0,0 +1,23 @@
+/*
+    Assignment Operators
+
+    =   ->  assignment operator
+    +=  ->  addition assignment operator
+    -=  ->  subtraction assignment operator
+    *=  ->  multiplication assignment operator
+    /=  ->  division assignment operator
+    %=  ->  remainder assignment operator
+    **= ->  exponentiation assignment operator
+*/
+
+let a = 10;
+
+a = a + 20;
+
+a = a + 70;
+
+a += 100; // a = a + 100
+
+a -= 50; // a = a - 50
+
+a /= 50; // a = a / 50
