21491cfab350f876a9c0d792d0484101e5d313a5
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-08-19 02:34:12 +0300

    Function Intro And Basic Usage

---
+++ 041_Function Intro And Basic Usage/script.js
@@ -0,0 +1,17 @@
+/*
+  Function
+  - What Is Function ? => Block Of Code That Can Be Reused
+  - User-Defined vs Built In 
+  - Syntax + Basic Usage 
+  - Example From Real Life
+  - Parameter + Argument
+  - Practical Example
+*/
+
+function sayHello(userName) {
+  console.log(`Hello ${userName}.`);
+}
+
+sayHello("Osama"); // Hello Osama.
+sayHello("Sayed"); // Hello Sayed.
+sayHello("Ali"); // Hello Ali.
